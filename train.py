e1433994f38cbaeb44e261169bc5dc6885474787
Marko Jocić <jocicmarko@gmail.com>
2016-06-04 20:33:26 +0200

    dice_coef() TF backend fix (Issue 1)

--- train.py
+++ train.py
@@ -19,7 +19,8 @@ smooth = 1.
 def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
-    return (2. * K.dot(y_true_f, K.transpose(y_pred_f)) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 
 def dice_coef_loss(y_true, y_pred):
